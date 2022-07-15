5a8dc1becea2ee408e9e7250b15a6fec30c20ee2
가영 <kykim097@gmail.com>
2022-07-15 20:24:11 +0900

    salePrice 예외 처리

--- src/main/kotlin/com/nhn/commerce/controller/ProductController.kt
+++ src/main/kotlin/com/nhn/commerce/controller/ProductController.kt
@@ -22,6 +22,7 @@ class ProductController(
     // TODO (상품 상세 조회 기능 + Exception 처리)
     @GetMapping("/product/detail/{productNo}")
     fun getProductDetail(model: Model, @PathVariable("productNo") productNo: Int): String {
+        ///// exception 처리...?
         val p: Product = productService.getProductDetail(productNo)
         if (p == null) {
             println("Exception")
@@ -32,6 +33,9 @@ class ProductController(
         return "detail"
     }
 
+
+    //////******** scope function + 확장 함수 사용 ㅜㅜ
+
     // TODO (상품 추가 기능)
     @GetMapping("/product/add")
     fun addProduct(): String {
@@ -41,23 +45,20 @@ class ProductController(
     }
 
     @PostMapping("/product/addProduct")
-    fun addProductWork(productName: String, salePrice: Int): String {
+    fun addProductWork(productName: String = "Default", salePrice: Int): String {
 
         productService.addProductWork(productName, salePrice, LocalDateTime.now())
+        //println(productName)
         println("와우 추가 완")
         return "redirect:/product"
     }
 
 
     // TODO (상품 수정 기능 + Exception 처리)
-
     @GetMapping("/product/update/{productNo}")
     fun updateProduct(model: Model, @PathVariable("productNo") productNo: Int): String {
-        val p: Product = productService.getProductDetail(productNo)
-        if (p == null) {
-            println("Exception")
-            return ("product")
-        }
+
+
         model.addAttribute("product", productService.getProductDetail(productNo))
         //println("수정 시작")
         return "update"
@@ -65,7 +66,9 @@ class ProductController(
 
 
     @PostMapping("/product/updateProduct")
-    fun updateProductWork(productName: String, salePrice: Int, productNo: Int): String {
+    fun updateProductWork(productName: String = "default", salePrice: Int, productNo: Int): String {
+        val p: Product = productService.getProductDetail(productNo)
+        
         productService.updateProductWork(productName, salePrice, LocalDateTime.now(), productNo)
         //println("업뎃 완~")
         return "redirect:/product"
@@ -81,6 +84,7 @@ class ProductController(
             println("Exception")
             return ("product")
         }
+
         productService.deleteProduct(productNo)
         return "redirect:/product"
     }
--- src/main/kotlin/com/nhn/commerce/service/ProductService.kt
+++ src/main/kotlin/com/nhn/commerce/service/ProductService.kt
@@ -13,14 +13,34 @@ class ProductService(
 
     fun getProductDetail(productNo: Int): Product = productRepository.getProductDetail(productNo)
 
-    fun addProductWork(productName: String, salePrice: Int, registerYmdt: LocalDateTime) =
+//    fun addProductWork(productName: String, salePrice: Int, registerYmdt: LocalDateTime) {
+//        productRepository.addProductWork(productName, salePrice, registerYmdt)
+//    }
+    fun addProductWork(productName: String, salePrice: Int, registerYmdt: LocalDateTime) {
+        if (salePrice.numCheck(salePrice)) {
+            throw Exception("가격이 0보다 작습니다. ")
+        }
         productRepository.addProductWork(productName, salePrice, registerYmdt)
+    }
+
+    fun Int.numCheck (salePrice:Int):Boolean {
+        if (salePrice < 0) {
+            return true
+        }
+        return false
+    }
 
     fun deleteProduct(productNo: Int) = productRepository.deleteProduct(productNo)
 
     //fun updateProductWork(productNo: Int, productName: String, salePrice: Int, registerYmdt: LocalDateTime) =
-    fun updateProductWork(productName: String, salePrice: Int, registerYmdt: LocalDateTime, productNo: Int) =
-        productRepository.updateProductWork(productName, salePrice, registerYmdt, productNo)
+//    fun updateProductWork(productName: String, salePrice: Int, registerYmdt: LocalDateTime, productNo: Int) =
+//        productRepository.updateProductWork(productName, salePrice, registerYmdt, productNo)
 
+    fun updateProductWork(productName: String, salePrice: Int, registerYmdt: LocalDateTime, productNo: Int) {
+        if (salePrice.numCheck(salePrice)) {
+            throw Exception("가격이 0보다 작습니다. ")
+        }
+        productRepository.addProductWork(productName, salePrice, registerYmdt)
+    }
 
 }
